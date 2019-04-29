01c0e8975c0c34f069d0e7e79c39330c4feff33e
mGuv <mattmcg.gamedev@gmail.com>
2019-04-29 19:25:12 +0100

    Make Islands name's an EventValue so they can be changed

--- src/Anno/Island/Island.ts
+++ src/Anno/Island/Island.ts
@@ -1,17 +1,18 @@
 import Dictionary from "../../Collections/Dictionary";
 import PopType from "../Population/PopType";
+import EventValue from "../../EventValue";
 
 class Island {
-    public name: string;
-    public population: Dictionary<PopType, number>;
+    public name: EventValue<string>;
+    public population: Dictionary<PopType, number>;    
 
-    public constructor(name: string, pops: Dictionary<PopType, number>) {
+    public constructor(name: EventValue<string>, pops: Dictionary<PopType, number>) {
         this.name = name;
         this.population = pops;
     }
 
     public toString = () => {
-        return this.name;
+        return this.name.getValue();
     }
 }
 
