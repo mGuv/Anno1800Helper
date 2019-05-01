cd2dfd175e42d37b643a28aecbbb95712b52ed4a
mGuv <mattmcg.gamedev@gmail.com>
2019-05-01 23:14:45 +0100

    Refactory Select Box to have a label

--- src/Components/Inputs/SelectBox/SelectBox.tsx
+++ src/Components/Inputs/SelectBox/SelectBox.tsx
@@ -9,6 +9,7 @@ import ReactResizeDetector from 'react-resize-detector';
 interface Props<T> {
     value: EventValue<T>,
     options: T[],
+    name?: string,
 }
 
 interface State<T> {
@@ -17,7 +18,7 @@ interface State<T> {
     width: number,
 }
 
-class SelectBox<T> extends React.PureComponent<Props<T>, State<T>> {
+class SelectBox2<T> extends React.PureComponent<Props<T>, State<T>> {
 
     public constructor(props: Props<T>) {
         super(props);
@@ -73,12 +74,20 @@ class SelectBox<T> extends React.PureComponent<Props<T>, State<T>> {
 
     public render(): JSX.Element {
 
+        const name: string = this.props.name || "";
+        const labelElement: JSX.Element | null = name.length > 0 ? (<div className="selectBox__label">{this.props.name}: </div>) : null;
+        const fillerElement: JSX.Element | null = name.length > 0 ? (<div className="filler"></div>) : null;
+
         if (!this.state.isOpen) {
             return (
                 <div className="selectBox__container" onClick={this.open}>
-                    <div className="label__container">
-                        <span className="value">{this.state.currentValue.toString()}</span>
-                        <span className="icon"><FontAwesomeIcon icon={faChevronDown} /></span>
+                    <div className="control__container">
+
+                        {labelElement}
+                        <div className="value__container">
+                            <span className="value">{this.state.currentValue.toString()}</span>
+                            <span className="icon"><FontAwesomeIcon icon={faChevronDown} /></span>
+                        </div>
                     </div>
                 </div>
             );
@@ -86,19 +95,23 @@ class SelectBox<T> extends React.PureComponent<Props<T>, State<T>> {
 
         return (
             <div className="selectBox__container">
-                <div>
-                    <div className="label__container" onClick={this.close}>
+                <div className="control__container">
+                    {labelElement}
+                    <div className="value__container">
                         <span className="value">{this.state.currentValue.toString()}</span>
-                        <span className="icon"><FontAwesomeIcon icon={faChevronUp} /></span>
+                        <span className="icon"><FontAwesomeIcon icon={faChevronDown} /></span>                    
                     </div>
                     <ReactResizeDetector handleWidth={true} handleHeight={true} onResize={this.setWidth} />
                 </div>
-                <div className="options__container">
+                <div className="options__container" style={{ width: this.state.width }}>
+                    {fillerElement}
+                    <div className="options">
                     {
                         this.props.options.map((value) => {
-                            return <div style={{ width: this.state.width }} className="option" key={value.toString()} onClick={() => { this.select(value) }}>{value.toString()}</div>;
+                            return <div className="option" key={value.toString()} onClick={() => { this.select(value) }}>{value.toString()}</div>;
                         })
                     }
+                    </div>
                 </div>
             </div>
         );
@@ -107,4 +120,4 @@ class SelectBox<T> extends React.PureComponent<Props<T>, State<T>> {
 
 
 
-export default handleClickOutside(SelectBox);
+export default handleClickOutside(SelectBox2);
